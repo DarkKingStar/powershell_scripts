930862bd07a3e0c0b6d238e6589b768fce79e073
unknown <sounakguha.12345@gmail.com>
2024-07-12 11:54:21 +0000

    if else

--- conditionalStatement/ques1.ps1
+++ conditionalStatement/ques1.ps1
@@ -2,14 +2,14 @@
 asked user to service name and my script will go to the backend and ask me the service is running or not.
 If running asked user to stopped it or cancel it or vice versa
 #>
-$serviceName = Read-Host "Please enter a service name"
-$service = Get-Service | Where-Object { $_.Name -eq $serviceName }
-
+$service = Read-Host "Please enter a service name" |`
+            Get-Service |`
+            Where-Object { $_.Name -eq $serviceName }
 if ($service) {
     $flagService = If($service.Status -eq 'Running') {1} Else {0}
     $startorstop = If($flagService) {"STOP"} Else {"START"}
     $response = Read-Host "$($service.status) - Do you want to $startorstop it? (y/n)"
-    if($response -eq 'y'){
+    if($response -eq 'y' -or $response -eq  'yes'){
         If($flagService){Stop-Service -Name $service.Name}else{Start-Service $service.Name}
     }
     $service0 = Get-Service -Name $serviceName
