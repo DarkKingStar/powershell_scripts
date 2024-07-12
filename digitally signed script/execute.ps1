fed94954c2480d387e093ed9c0d28601365d6c6a
unknown <sounakguha.12345@gmail.com>
2024-07-12 07:07:01 +0000

    digital signed certificate

--- digitally signed script/execute.ps1
+++ digitally signed script/execute.ps1
@@ -1,1 +1,7 @@
-Get-Command *pfx*
+#Get-Command *pfx*
+
+#Get-Help Set-AuthenticodeSignature -Online
+
+$cert = Get-ChildItem Cert:\CurrentUser\My\1DECA67B01F669A1C3735145E0CADBF6070D3E77
+$filepath = 'C:\Users\sounak\Desktop\practice\digitally signed script\script.ps1'
+Set-AuthenticodeSignature -FilePath $filepath $cert
