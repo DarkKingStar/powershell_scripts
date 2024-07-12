1881dceac084f7aac2664450b8307799875032b8
unknown <sounakguha.12345@gmail.com>
2024-07-12 08:46:06 +0000

    digital signed certificate

--- digitally signed script/bindingExecute.ps1
+++ digitally signed script/bindingExecute.ps1
@@ -5,3 +5,6 @@
 $cert = Get-ChildItem Cert:\CurrentUser\My\1DECA67B01F669A1C3735145E0CADBF6070D3E77
 $filepath = 'C:\Users\sounak\Desktop\practice\digitally signed script\script.ps1'
 Set-AuthenticodeSignature -FilePath $filepath $cert
+
+# display the results
+Get-AuthenticodeSignature -filepath 'C:\Users\sounak\Desktop\practice\digitally signed script\script.ps1'
