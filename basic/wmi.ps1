bee1c2bc1ba9c886abeefc4b0d2a91fcd205e8a3
unknown <sounakguha.12345@gmail.com>
2024-07-13 11:24:21 +0000

    wmi

--- basic/wmi.ps1
+++ basic/wmi.ps1
@@ -1,9 +1,9 @@
-# Write-Host "total no of WMIObject is:"
-# $(Get-WmiObject -List | Measure-Object).Count
-# Write-Host "total no of WMIObject that start with win32 is:"
-# $(Get-WmiObject -List | Where-Object {$_.name -match "^win32_"} | Measure-Object).Count
+Write-Host "total no of WMIObject is:"
+$(Get-WmiObject -List | Measure-Object).Count
+Write-Host "total no of WMIObject that start with win32 is:"
+$(Get-WmiObject -List | Where-Object {$_.name -match "^win32_"} | Measure-Object).Count
 
-# $(Get-WmiObject -List | Where-Object {($_.name -match "^win32_") -and ($_.name -like "*bios*")})
+$(Get-WmiObject -List | Where-Object {($_.name -match "^win32_") -and ($_.name -like "*bios*")})
 
 Get-WmiObject -Class win32_bios
 Get-WmiObject -Class win32_computersystem
